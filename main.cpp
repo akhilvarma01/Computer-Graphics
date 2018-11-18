0a94a316ba28d05193e3702230f82cfe4110ad75
Akhil Varma Alluri <masters19951021@mail.fresnostate.edu>
2018-11-17 19:39:22 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-/* Name - AKHIL VARMA ALLURI
-*
-* Project - Modelling Transformations
-*
-* CSCI272 - Computer Graphics
-*
-* Date of Submission - 10/20/18
+/*
+* Submitted by Akhil Varma Alluri
+* CSCI 272
+* Project 3
+* Submitted to Dr.Dhanyu
 */
 #include <string.h>
 
@@ -14,38 +12,34 @@
 #include <GL/glut.h>
 #endif
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-//#include <GL/freeglut.h>
+#include <math.h>
+
+#define PI 3.14159
+#define max_ctlpts 14
 
 using namespace std;
 
-static int slices = 50; static int stacks = 50; // defined second and third coordinates of sphere
-
-double xsphere = -3.5;
-double ysphere= 0.0;
-double zsphere = -1.0;                  //given coordinates mentioned in assignment
-double radius = 1.3;
-double scaleSphere = 1.0;
-
-double xcube = 4.0;
-double ycube = 0.0;
-double zcube = -2.0;
-double scaleCube = 1.0;
-
-double xtea = 0.0 ;
-double ytea =0.0;
-double ztea = -3.0;
-double scaleTeapot = 1.0;
-
-
-bool sval =0, cval =0, tval =0;        //boolean value flags to check the key pressed.
-
-double rotSphere = 0.0;
-double rotCube = 0.0;
-double rotTea = 0.0;
-
 bool WireFrame= false;
+
+float xp[max_ctlpts],yp[max_ctlpts],zp[max_ctlpts];
+float p,q; //for x&y mouse pts
+float t;
+float i=0;
+bool flag=false;
+
+int cpts = 0; //count control points
+float xpos =0;
+float ypos =0;
+float zpos =0;
+float newAngle =0;
+//float n=0;
+
+float z =-3.0;
+
+//bool val = 0;
 
 const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
 const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
@@ -58,25 +52,55 @@ const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat high_shininess[] = { 100.0f };
 
 
+float Wwidth,Wheight;
+
 /* GLUT callback Handlers */
 
 static void resize(int width, int height)
 {
      double Ratio;
 
-   if(width<=height)
-     {
-         glViewport(0,(GLsizei) (height-width)/2,(GLsizei) width,(GLsizei) width);
-      }
-    else
-      {
-        glViewport((GLsizei) (width-height)/2 ,0 ,(GLsizei) height,(GLsizei) height);
-      }
+     Wwidth = (float)width;
+     Wheight = (float)height;
+
+     Ratio= (double)width /(double)height;
+
+    glViewport(0,0,(GLsizei) width,(GLsizei) height);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-	gluPerspective (50.0f,1,0.1f, 100.0f);
+
+	gluPerspective (45.0f,Ratio,0.1f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
  }
+
+//implementing formula
+
+float fact(float num)
+{
+
+    if(num==1 || num==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (num*fact(num-1));
+    }
+}
+
+//binomial coefficient logic
+
+float bin_coeff(float n,float i)
+{
+    float val;
+    val= fact(n) / (fact(i)*fact(n-i));
+    return val;
+}
+
+//display function
 
 static void display(void)
 {
@@ -84,139 +108,170 @@ static void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    GLdouble size = 0.5;
+    gluLookAt(0,0,10.0,0.0,0.0,0.0,0.0,1.0,100.0);
 
-    gluLookAt(0,5,10,0.0,0.0,0.0,0.0,1.0,0.0);
+    glRotatef(newAngle,0.0,1.0,0.0);  //used for rotation around y axis
 
     if(WireFrame)
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);		//Draw Our Mesh In Wireframe Mesh
 	else
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);		//Toggle WIRE FRAME
 
-	glPushMatrix();           //Cube.
-        glColor3f(0,1,0);
-        glTranslatef(xcube,ycube,zcube);
-        glRotatef(rotCube,0,1,0);                       //functions for translate , rotate and scale for all the three figures.
-        glScalef(scaleCube,scaleCube,scaleCube);
-        glutSolidCube(2.0);
 
-    glPopMatrix();
 
-        glPushMatrix();       //Sphere.
-        glColor3f(0,0,1);
-        glTranslated(xsphere,ysphere,zsphere);
-        glRotated(rotSphere,0,1,0);
-        glScalef(scaleSphere,scaleSphere,scaleSphere);
-        glutSolidSphere(radius,slices,stacks);
+ glPointSize(5);
+ glBegin(GL_POINTS);
 
-    glPopMatrix();
+//logic for togging points on and off
 
-        glPushMatrix ();    //Teapot.
-        glColor3f(1,0,0);
-        glTranslated(xtea,ytea,ztea);
-        glRotated(rotTea,0,1,0);
-        glScalef(scaleTeapot,scaleTeapot,scaleTeapot);
-        glutSolidTeapot ( 1.5 );
+ if(flag==false)
+ {
+     for(int i=0; i<cpts;i++)
+     {
+         glVertex3f(xp[i],yp[i],zp[i]);
+     }
+ }
+ else if(flag==true)
+ {
+     for(int i=0;i<cpts;i++)
+     {
 
-        //glColor3f(1,0,0);
-    glPopMatrix ();
+     }
+ }
+glEnd();
 
-    glutSwapBuffers();
+//logic for bezier curve implementation
+
+ glPointSize(2);
+ glColor3f(1.0,1.0,1.0);
+
+ if(cpts>2)
+ {
+    for(int j=3;j<=cpts;j++)
+    {
+     for(t=0;t<=1;t=t+0.005)
+     {
+         for(int i=0;i<j;i++)
+         {
+             xpos += bin_coeff(j-1,i)*pow(t,i)*pow(1-t,j-1-i)*xp[i];
+             ypos += bin_coeff(j-1,i)*pow(t,i)*pow(1-t,j-1-i)*yp[i];
+             zpos += bin_coeff(j-1,i)*pow(t,i)*pow(1-t,j-1-i)*zp[i];
+
+         }
+         glBegin(GL_POINTS);
+         glVertex3f(xpos,ypos,zpos);
+         glEnd();
+         xpos=0;
+         ypos=0;
+         zpos=0;
+     }
+  }
+ }
+
+
+glutSwapBuffers();
 }
+
 
 static void key(unsigned char key, int x, int y)
 {
-
     switch (key)
     {
-        case 27 :
-
-        case 'q':
+        case 27 : //esc
+        case 32 : //space bar
+            cpts=0;
+            glutPostRedisplay();
+            break;
+        case 'q':  //exit
             exit(0);
-    break;
+            break;
 
-        case 'c' : //
-        cval = true;
-        sval = false;                   // boolean values declared for each figure respectively
-        tval = false;
-
-    break;
-
-        case 's' : //
-        sval = true;
-        tval = false;
-        cval = false;
-
-    break;
-        case 't': //
-        tval = true;
-        sval = false;
-        cval = false;
-
-    break;
-
-      case 'w':
-        WireFrame =!WireFrame;
-        break;
+	  case 'w':
+		WireFrame =!WireFrame;
+	       break;
     }
 }
 
+//logic for turning the diagram with a certain angle
 void Specialkeys(int key, int x, int y)
 {
     switch(key)
     {
-    case GLUT_KEY_UP: // When upkey is pressed, figure zooms.
-       if(cval){
-       scaleCube += 0.5;
-       }else if(sval){
-       scaleSphere += 0.5;
-       }else if(tval){
-       scaleTeapot += 0.5;
-       }
+      case GLUT_KEY_RIGHT:
+            {
+                newAngle +=10;
+            }
+            break;
 
-    break;
-    case GLUT_KEY_DOWN:  // When downkey is pressed, figure compresses.
-         if(cval){
-         scaleCube -= 0.5;
-         }else if(sval){
-         scaleSphere -= 0.5;
-         }else if(tval){
-         scaleTeapot -= 0.5;
-         }
-
-     break;
-    case GLUT_KEY_LEFT: // When leftkey is pressed , figure increments 5 degrees.
-         if(cval){
-         rotCube = rotCube+5;
-         }else if(sval){
-         rotSphere = rotSphere+5;
-         }else if(tval){
-         rotTea = rotTea+5;
-         }
-
-     break;
-      case GLUT_KEY_RIGHT:  // When right key is pressed , figure decrements 5 degrees.
-         if(cval){
-        rotCube = rotCube-5;
-         }else if(sval){
-        rotSphere = rotSphere-5;
-         }else if(tval){
-        rotTea=rotTea-5;
-         }
-
-     break;
-}
-
+         case GLUT_KEY_LEFT:
+            {
+                newAngle -=10;
+            }
+            break;
+   }
   glutPostRedisplay();
 }
 
+
 static void idle(void)
 {
-    glutPostRedisplay();
+    // Use parametric equation with t increment for xpos and ypos
+    // Don't need a loop
+     glutPostRedisplay();
 }
+
+//logic for mouse button registration
+
+void mouse(int btn, int state, int x, int y){
+
+    float scale = 54*(Wwidth/Wheight);
+
+     printf(" original %d %d %d \n", x,y,z);
+    switch(btn){
+
+        case GLUT_LEFT_BUTTON:
+
+        if(state==GLUT_DOWN){
+                    if(cpts<15)
+                    {
+                    p = (x -(Wwidth/2))/scale; // Get the mouse x and y coordinate, and translate based on origin.
+                    q = ((Wheight/2) - y)/scale;
+
+                    xp[cpts] = p;
+                    yp[cpts] = q;
+                    zp[cpts] = z;
+                    z+=0.2;
+                    cpts++;
+                    }
+                }
+            break;
+
+        case GLUT_RIGHT_BUTTON:
+            {
+                if(state==GLUT_DOWN){
+                    if(flag==false){
+                    flag=true;
+                    }
+
+                    else
+                    {
+                        flag=false;
+                    }
+              }
+                }
+             break;
+}
+  glutPostRedisplay();
+};
+
+
 
 static void init(void)
 {
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glClearColor(0.5f, 0.5f, 1.0f, 0.0f);                 // assign a color you like
 
     glEnable(GL_NORMALIZE);
     glEnable(GL_COLOR_MATERIAL);
@@ -240,7 +295,6 @@ static void init(void)
     glEnable(GL_LIGHTING);
 }
 
-
 /* Program entry point */
 
 int main(int argc, char *argv[])
@@ -248,17 +302,19 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
 
     glutInitWindowSize(800,600);
-    glutInitWindowPosition(10,10);
+    glutInitWindowPosition(0,0);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("GLUT Shapes");
+    glutCreateWindow("Project Assignment 3");
     init();
     glutReshapeFunc(resize);
     glutDisplayFunc(display);
+    glutMouseFunc(mouse);
     glutKeyboardFunc(key);
-    glutSpecialFunc(Specialkeys);
+   glutSpecialFunc(Specialkeys);
 
     glutIdleFunc(idle);
+
     glutMainLoop();
 
     return EXIT_SUCCESS;
